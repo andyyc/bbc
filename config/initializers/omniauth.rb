51ed948d34f69d001a60756836bab25964f8bbef
Andy Chen <andy.y.c@gmail.com>
2011-12-02 23:33:08 -0800

    changed omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '157311767691429', 'bcb8f02ffedf4764cddf9ea8b2f338e7'
-  #provider :facebook, '145412355562649', '67ef6f9d1eaef290be340a9f53fa090e'
+  #provider :facebook, '157311767691429', 'bcb8f02ffedf4764cddf9ea8b2f338e7'
+  provider :facebook, '145412355562649', '67ef6f9d1eaef290be340a9f53fa090e'
 end
