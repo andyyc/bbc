85d00d8ac9435ed94bbe3f1cf9bdda8015a39ddf
Andy Chen <andy.y.c@gmail.com>
2011-12-05 00:54:00 -0800

    added debug statement

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -7,6 +7,7 @@ class Authorization < ActiveRecord::Base
     puts 'find from hash'
     puts hash
     authorization = find_by_provider_and_uid(hash['provider'], hash['uid'])
+    puts authorization
     unless authorization.nil?
       authorization.update_from_hash(hash)
     end
@@ -17,11 +18,12 @@ class Authorization < ActiveRecord::Base
     puts 'create from hash'
     puts hash
     user ||= User.create_from_hash!(hash)
-    
+    puts user
     Authorization.create(:user => user, :uid => hash['uid'], :provider => hash['provider'], :token => hash['credentials']['token'], :first_name => hash['info']['first_name'], :last_name => hash['info']['last_name'], :name => hash['info']['name'], :nickname => hash['info']['nickname'])
   end
 
   def update_from_hash(hash)
+    puts "update from hash"
     self.token = hash['credentials']['token']
     self.first_name = hash['info']['first_name']
     self.last_name = hash['info']['last_name']
